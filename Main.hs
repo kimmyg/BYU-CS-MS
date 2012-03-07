ffba9530200947b23e7fbda939d3b3d7f0d954b4
Kimball Germane <krgermane@yahoo.com>
2012-03-07 09:12:07 -0700

    Getting to the core of the issue.

--- Main.hs
+++ Main.hs
@@ -17,13 +17,19 @@ fact :: Stack Frame -> Integer -> ([Value], Integer)
 fact fs 0 = (ccm fs "fact", 1)
 fact fs n = wcm fs "fact" (show n) (\fs -> \n -> let (ms, acc) = fact fs (n - 1) in (ms, n * acc)) n
 
+--fact_tr_inner :: Stack Frame -> Integer -> Integer -> ([Value], Integer)
+--fact_tr_inner fs 0 acc = (ccm fs "fact", acc)
+--fact_tr_inner fs n acc = wcm fs "fact" (show n) 
 
+--fact_tr :: Stack Frame -> Integer -> ([Value], Integer)
+--fact_tr n = let fact_tr_iter n a
+ 
 main = print $ fact [(Frame [])] 3
 
 {-
 fact 0 = do {
-	ccm "fact";
-	1
+	cms <- ccm "fact";
+	return? (cms, 1)
 }
 
 fact n = do {
@@ -32,12 +38,38 @@ fact n = do {
 }
 
 fact_tr 0 acc = do {
-	ccm "fact";
-	acc
+	cms <- ccm "fact";
+	return? (cms, acc)
 }
 
 fact_tr n acc = do {
 	wcm "fact" (show n);
 	fact_tr (n - 1) (n * acc)
 }
+
+constraints derived from the above:
+cms :: CMM A -> Key -> [Value] in some way that works with bind
+maybe we run it with a set of continuations already
+a continuation mark monad is a computation with an associated set of continuation marks
+
+runCMM :: CMM A -> Stack Frame -> something
+a bind should add a continuation mark
+a return is empty continuation marks with the identity function?
+
+return :: a -> CMM a
+return x = \s -> ([]
+
+m >>= f means...
+
+
+
+do {
+	x <- return n;
+	wcm "fact" (show n);
+	z <- ccm "fact"
+
+
+
+wcm ... key mark g... = \x -> g (frameSet f key mark) x
+
 -}
