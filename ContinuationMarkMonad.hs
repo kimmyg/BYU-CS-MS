de3460cbbb50daf0da47c75fb76bbdcbd0c818af
Kimball Germane <krgermane@yahoo.com>
2012-03-10 21:37:03 -0700

    Opaqueness.

--- ContinuationMarkMonad.hs
+++ ContinuationMarkMonad.hs
@@ -8,15 +8,19 @@ instance Monad ((->) a) where
   return x = \_ -> x
   r >>= f = \e -> f (r e) e
 
-call :: ((Stack Frame) -> a) -> (Stack Frame) -> a
+type CM = Stack Frame
+
+call :: (CM -> a) -> CM -> a
 call r fs = r ((Frame []):fs)
 
-wcm :: Key -> Value -> ((Stack Frame) -> a) -> (Stack Frame) -> a
+wcm :: Key -> Value -> (CM -> a) -> CM -> a
 wcm k v r (f:fs) = r ((frameSet f k v):fs)
 
-ccm :: (Stack Frame) -> Key -> [Value]
+ccm :: CM -> Key -> [Value]
 ccm []     k = []
 ccm (f:fs) k = case (frameGet f k) of
   Nothing -> ccm fs k
   Just v  -> v:(ccm fs k)
 
+getCM :: CM
+getCM = [(Frame [])]
--- Main.hs
+++ Main.hs
@@ -1,16 +1,13 @@
 module Main where
 import ContinuationMarkMonad
-import Mark
-import Frame
-import Stack
 
-fact :: Int -> (Stack Frame) -> Int
+fact :: Int -> CM -> Int
 fact 0 fs = 1
 fact n fs = wcm "fact" (show n) (\fs -> n * (call (fact (n - 1)) fs)) fs
 
-fact_tr :: Int -> Int -> (Stack Frame) -> Int
+fact_tr :: Int -> Int -> CM -> Int
 fact_tr 0 acc fs = acc
 fact_tr n acc fs = wcm "fact" (show n) (fact_tr (n - 1) (n * acc)) fs
 
-main = print $ fact_tr 4 1 [(Frame [])]
+main = print $ fact_tr 4 1 getCM
 
