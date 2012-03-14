49f644864dfdec8e967bc786f1b6f4245dbecaf9
Kimball Germane <krgermane@yahoo.com>
2012-03-14 09:47:40 -0600

    Newtype is what we want.

--- ContinuationMarkMonad.hs
+++ ContinuationMarkMonad.hs
@@ -4,23 +4,23 @@ import Mark
 import Frame
 import Stack
 
-type CM k v a = (Stack (Frame k v)) -> a
+newtype CM k v a = CM ((Stack (Frame k v)) -> a)
 
-instance Monad ((->) [[(k, v)]]) where
-  return x = \_ -> x
-  r >>= f = \e -> f (r e) e
+instance Monad (CM k v) where
+  return x = CM (\_ -> x)
+  r >>= f = CM (\fs -> let CM g = let CM h = r in f (h fs) in g fs)
 
-call :: (CM a) -> (CM a)
+call :: (CM k v a) -> (Stack (Frame k v)) -> a
 call r fs = r ([]:fs)
 
-wcm :: (Eq k) => k -> v -> (CM a) -> (CM a)
+wcm :: (Eq k) => k -> v -> (CM k v a) -> (Stack (Frame k v)) -> a
 wcm k v r (f:fs) = r ((frameSet f k v):fs)
 
-ccm :: (Eq k) => k -> (CM a) -> [v]
+ccm :: (Eq k) => k -> (CM k v a) -> [v]
 ccm k []     = []
 ccm k (f:fs) = case (frameGet f k) of
   Nothing -> ccm k fs
   Just v  -> v:(ccm k fs)
 
-getCM :: CM a
+getCM :: CM k v a
 getCM = [[]]
