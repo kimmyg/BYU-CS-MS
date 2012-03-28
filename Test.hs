ff4577fbf6763a4a8c26c44488428baeafc9753b
Kimball Germane <krgermane@yahoo.com>
2012-03-28 15:13:44 -0600

    Updated tests.

--- Test.hs
+++ Test.hs
@@ -1,41 +1,28 @@
 module Main where
 import ContinuationMarkMonad
 
-test1 :: [String]
-test1 fs = do
-  ccm "test" (return 0)
+test1 :: CM String String [String]
+test1 = ccm "test"
 
 test1correct :: [String]
 test1correct = []
 
-test2 :: CM String String -> [String]
-test2 fs = do
-  wcm "test" "value" test1
+test2 :: CM String String [String]
+test2 = wcm "test" "value" test1
 
 test2correct :: [String]
 test2correct = ["value"]
 
-test3 :: CM String String -> [String]
-test3 fs = (do {
-  wcm "test" "value" test1;
+test3 :: CM String String [String]
+test3 = do
+  wcm "test" "value" test1
   ccm "test"
-}) fs
 
 test3correct :: [String]
 test3correct = []
 
-fact :: Int -> CM String String Int
-fact 0 = return 1
-fact n = wcm "fact" (show n) (do
-  acc <- fact (n - 1)
-  return (n * acc))
-
-fact_tr :: Int -> Int -> CM String String Int
-fact_tr 0 acc = return acc
-fact_tr n acc = wcm "fact" (show n) (fact_tr (n - 1) (n * acc))
-
-test :: [((CM String String) -> [String], [String])] -> [Bool]
+test :: [((CM String String [String]), [String])] -> [Bool]
 test [] = []
-test ((f, e):ts) = (f getCM == e):(test ts)
+test ((m, e):ts) = (runCM m == e):(test ts)
 
 main = print $ test [(test1, test1correct), (test2, test2correct), (test3, test3correct)]
