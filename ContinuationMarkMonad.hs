4cf106aa0c4dd79bdaa637a5537a5564d7672925
Kimball Germane <krgermane@yahoo.com>
2012-03-28 12:51:04 -0600

    Done! Now I just need to clean up the code (in many ways).

--- ContinuationMarkMonad.hs
+++ ContinuationMarkMonad.hs
@@ -10,7 +10,7 @@ runCM (CM m) = m [empty]
 
 instance Monad (CM k v) where
   return x = CM (\_ -> x)
-  CM m >>= f = CM (\fs -> let CM m' = f (m fs) in m' (empty:fs))
+  CM m >>= f = CM (\fs -> let CM m' = f (m (empty:fs)) in m' fs)
 
 wcm :: Ord k => k -> v -> CM k v a -> CM k v a
 wcm k v (CM m) = CM (\(f:fs) -> m ((insert k v f):fs))
@@ -21,8 +21,11 @@ frameMarks (k:ks) m = case Data.Map.lookup k m of
   Just v  -> (k, v):(frameMarks ks m)
   Nothing -> frameMarks ks m
 
-ccms :: Ord k => [k] -> CM k v a -> CM k v [[(k, v)]]
-ccms ks (CM m) = CM (\fs -> Prelude.map (\f -> frameMarks ks f) fs)
+ccms :: Ord k => [k] -> CM k v [[(k, v)]]
+ccms ks = CM (\fs -> Prelude.filter (not . Prelude.null) (Prelude.map (\f -> frameMarks ks f) fs))
 
---ccm :: Ord k => k -> CM k v a -> CM k v [v]
---ccm k (CM m) = CM (\fs -> let Prelude.map (\[(_, v)] -> v) (ccms [k] cm)
+ccm :: Ord k => k -> CM k v [v]
+ccm k = CM (\fs -> let CM m = ccms [k] in Prelude.map (\[(_, v)] -> v) (m fs))
+
+getallCM :: CM k v (Stack (Map k v))
+getallCM = CM (\fs -> fs)
--- Main.hs
+++ Main.hs
@@ -1,14 +1,25 @@
 module Main where
 import ContinuationMarkMonad
+import Data.Map
+import Stack
 
-fact :: Int -> CM String String Int
-fact 0 = return 1
+fact :: Int -> CM String String (Int, [String])
+fact 0 = do
+  ms <- ccm "fact"
+  return (1, ms)
+
 fact n = wcm "fact" (show n) (do
-  acc <- fact (n - 1)
-  return (n * acc))
+  (acc, ms) <- fact (n - 1)
+  return ((n * acc), ms))
 
-fact_tr :: Int -> Int -> CM String String Int
-fact_tr 0 acc = return acc
+fact_tr :: Int -> Int -> CM String String (Int, [String])
+fact_tr 0 acc = do
+  ms <- ccm "fact"
+  return (acc, ms)
+
 fact_tr n acc = wcm "fact" (show n) (fact_tr (n - 1) (n * acc))
 
-main = print $ runCM $ ccms ["fact"] $ fact_tr 5 1
+main = print $ runCM $ fact_tr 3 1
+
+--main = print $ runCM $ getallCM $ (return 1 :: CM String String Int)
+  
