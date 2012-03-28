4558864aaf05cd2a27a872c3a62d1aed97291ae6
Kimball Germane <krgermane@yahoo.com>
2012-03-28 15:32:20 -0600

    Updated tests.

--- Test.hs
+++ Test.hs
@@ -1,28 +1,35 @@
 module Main where
 import ContinuationMarkMonad
 
-test1 :: CM String String [String]
-test1 = ccm "test"
+test1 :: Bool
+test1 = null $ runCM $ ccm "test"
 
-test1correct :: [String]
-test1correct = []
+test2 :: Bool
+test2 = (==) ["value"] $ runCM $ wcm "test" "value" (ccm "test")
 
-test2 :: CM String String [String]
-test2 = wcm "test" "value" test1
-
-test2correct :: [String]
-test2correct = ["value"]
-
-test3 :: CM String String [String]
-test3 = do
-  wcm "test" "value" test1
+test3 :: Bool
+test3 = null $ runCM $ do
+  wcm "test" "value" (ccm "test")
   ccm "test"
 
-test3correct :: [String]
-test3correct = []
+fact :: Int -> CM String String (Int, [String])
+fact 0 = do
+  ms <- ccm "fact"
+  return (1, ms)
+fact n = wcm "fact" (show n) (do
+  (acc, ms) <- fact (n - 1)
+  return (n * acc, ms))
 
-test :: [((CM String String [String]), [String])] -> [Bool]
-test [] = []
-test ((m, e):ts) = (runCM m == e):(test ts)
+test4 :: Bool
+test4 = (==) (120, ["1", "2", "3", "4", "5"]) $ runCM $ fact 5
 
-main = print $ test [(test1, test1correct), (test2, test2correct), (test3, test3correct)]
+fact_tr :: Int -> Int -> CM String String (Int, [String])
+fact_tr 0 acc = do
+  ms <- ccm "fact"
+  return (acc, ms)
+fact_tr n acc = wcm "fact" (show n) (fact_tr (n - 1) (n * acc))
+
+test5 :: Bool
+test5 = (==) (120, ["1"]) $ runCM $ fact_tr 5 1
+
+main = print $ [test1, test2, test3, test4, test5]
