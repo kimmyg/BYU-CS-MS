bd2a483cbd51707085d74cc9fea2fd032b47a51b
Kimball Germane <krgermane@yahoo.com>
2012-04-30 13:27:14 -0600

    Experimenting with CM1.

--- CM1Test.hs
+++ CM1Test.hs
@@ -1,13 +1,18 @@
 module Main where
 import CM1
 
-fact :: Int -> CM1 String ([String], Int)
+fact :: Int -> CM1 String (Int,[String])
 fact 0 = do
-  marks <- ccm
-  return (marks, 1)
+  ms <- ccm
+  return (1,ms)
 
 fact n = do
-  (marks, acc) <- wcm (show n) (fact (n - 1));
-  return (marks, (n * acc))
-           
-main = let x = (wcm 2 ccm) in print $ runCM $ (wcm 1 (x >>= return))
+  (acc,ms) <- fact
+
+liftM2 (*) (return n) (wcm (show n) (fact (n - 1)))
+
+main = print $ runCM $ fact 5
+         
+--main = let x = (wcm 2 ccm) in (do
+--  print $ runCM $ (wcm 1 (x >>= return))
+--  print $ runCM $ (wcm 1 x))
