9ba4b849fd460141231de10be61f4ef6bcfebdcd
Kimball Germane <krgermane@yahoo.com>
2012-05-01 15:33:21 -0600

    Experimenting with new implementation.

--- code/CM1.hs
+++ code/CM1.hs
@@ -4,27 +4,24 @@ import Data.Maybe
 
 -- represents a computation with annotations (unkeyed)
 
-data CM1 m a = CM1 ([Maybe m] -> a)
+data CM1 m a = CM1 ([Maybe m] -> a) | CM1TP ([Maybe m] -> a)
 
 instance Monad (CM1 m) where
-  return x = CM1 (\_ -> x)
-  (CM1 m) >>= f = CM1 (\vs -> let (CM1 m') = f (m vs) in m' vs)
-
-liftM1 :: (a -> b) -> CM1 m a -> CM1 m b
-liftM1 f (CM1 m_a) = CM1 (\vs ->
-  let x = m_a (Nothing:vs) in
-    f x)
-
-liftM2 :: (a -> b -> c) -> CM1 m a -> CM1 m b -> CM1 m c
-liftM2 f (CM1 m_a) (CM1 m_b) = CM1 (\vs ->
-  let x = m_a (Nothing:vs) in
-    let y = m_b (Nothing:vs) in
-      f x y)
+  return x = CM1TP (\_ -> x)
+  (CM1 m) >>= f = case f (m (Nothing:[])) of
+    (CM1 _)   -> CM1 (\vs -> let (CM1 m') = f (m (Nothing:vs)) in m' vs)
+    (CM1TP _) -> CM1TP (\vs -> let (CM1TP m') = f (m vs) in m' vs)
+  (CM1TP m) >>= f = case f (m (Nothing:[])) of
+    (CM1 _)   -> CM1 (\vs -> let (CM1 m') = f (m (Nothing:vs)) in m' vs)
+    (CM1TP _) -> CM1TP (\vs -> let (CM1TP m') = f (m vs) in m' vs)
 
 wcm :: m -> CM1 m a -> CM1 m a
-wcm v (CM1 m) = CM1 (\(_:vs) -> m ((Just v):vs))
+wcm v (CM1 m)   = CM1 (\(_:vs) -> m ((Just v):vs))
+wcm v (CM1TP m) = CM1TP (\(_:vs) -> m ((Just v):vs))
 
 ccm :: CM1 m [m]
 ccm = CM1 catMaybes
 
-runCM (CM1 m) = m (Nothing:[])
+runCM :: CM1 m a -> a
+runCM (CM1 m)   = m (Nothing:[])
+runCM (CM1TP m) = m (Nothing:[])
--- code/CM1Test.hs
+++ code/CM1Test.hs
@@ -3,16 +3,27 @@ import CM1
 
 fact :: Int -> CM1 String (Int,[String])
 fact 0 = do
-  ms <- ccm
+  ms <- ccm;
   return (1,ms)
 
 fact n = do
-  (acc,ms) <- fact
+  (acc,ms) <- wcm (show n) (fact (n - 1))
+  return (n * acc,ms)
 
-liftM2 (*) (return n) (wcm (show n) (fact (n - 1)))
+fact_tr :: Int -> Int -> CM1 String (Int,[String])
+fact_tr 0 acc = do
+  ms <- ccm
+  return (acc,ms)
 
-main = print $ runCM $ fact 5
-         
---main = let x = (wcm 2 ccm) in (do
---  print $ runCM $ (wcm 1 (x >>= return))
---  print $ runCM $ (wcm 1 x))
+fact_tr n acc = wcm (show n) (fact_tr (n - 1) (n * acc))
+
+main = let x = (wcm 2 ccm) in do
+  putStrLn "tail recursive"
+  print $ runCM $ fact_tr 5 1
+  putStrLn "not tail-recursive"
+  print $ runCM $ fact 5
+  putStrLn "unbound"
+  print $ runCM $ (wcm 1 x)
+  putStrLn "bound (should be same as above)"
+  print $ runCM (wcm 1 (x >>= return))
+
