d7dc2d468073cfc3560daa9a32398cea46017b0e
Kimball Germane <krgermane@yahoo.com>
2012-05-07 14:24:37 -0600

    A little cleaner conceptually; a little more like Danvy's.

--- code/CM1.hs
+++ code/CM1.hs
@@ -4,19 +4,25 @@ import Data.Maybe
 
 -- represents a computation with annotations (unkeyed)
 
-data CM1 m a = CM1 ([Maybe m] -> a)
+data CM1 m a = CM1 ((Maybe m,[m]) -> a)
 
 instance Monad (CM1 m) where
   return x = CM1 (\_ -> x)
   (CM1 m) >>= f = CM1 (\vs -> let (CM1 m') = f (m vs) in m' vs)
 
 wcm :: m -> CM1 m a -> CM1 m a
-wcm v (CM1 m) = CM1 (\(_:vs) -> m ((Just v):vs))
+wcm v (CM1 m) = CM1 (\(_,vs) -> m (Just v,vs))
 
 ccm :: CM1 m [m]
-ccm = CM1 catMaybes
+ccm = CM1 (\(mv,vs) ->
+  case mv of
+    Just v  -> (v:vs)
+    Nothing -> vs)
 
 call :: CM1 m a -> CM1 m a
-call (CM1 m) = CM1 (\vs -> m (Nothing:vs))
+call (CM1 m) = CM1 (\(mv,vs) -> 
+  case mv of
+    Just v  -> m (Nothing,v:vs)
+    Nothing -> m (Nothing,vs))
 
-runCM (CM1 m) = m (Nothing:[])
+runCM (CM1 m) = m (Nothing,[])
